2e0d915667133962f7036ed9aab3a79fadf7193a
albim72 <marcin.albiniak72@gmail.com>
2026-02-04 10:13:42 +0100

    Add Prostokat class usage to main.py

--- DZIEN_3/FIGURY_ABSTRACT/main.py
+++ DZIEN_3/FIGURY_ABSTRACT/main.py
@@ -1,4 +1,13 @@
 from trojkat import Trojkat
+from prostokat import Prostokat
 
 tr = Trojkat(5,7)
 print(f"pole figry: {tr.__class__.__name__} wynosi: {tr.oblicz_pole():.2f} cm2")
+
+pr1 = Prostokat(3.5,4.7)
+print(f"pole figry: {pr1.__class__.__name__} wynosi: {pr1.oblicz_pole():.2f} cm2")
+print(f"typ obiektu pr1: {type(pr1)}")
+
+pr2 = Prostokat(5.1,5.1)
+print(f"pole figry: {pr2.__class__.__name__} wynosi: {pr2.oblicz_pole():.2f} cm2")
+print(f"typ obiektu pr2: {type(pr2)}")
