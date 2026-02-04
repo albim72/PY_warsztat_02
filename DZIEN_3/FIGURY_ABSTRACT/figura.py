c49144962a3dc25a05f8240678afa2f2f2e477c4
albim72 <marcin.albiniak72@gmail.com>
2026-02-04 09:51:30 +0100

    Update figura.py

--- DZIEN_3/FIGURY_ABSTRACT/figura.py
+++ DZIEN_3/FIGURY_ABSTRACT/figura.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
 
 class Figura(ABC):
-    
+
     def __init__(self, a, b):
         self.a = a
         self.b = b
-    
+        self.info()
+
     @abstractmethod
     def oblicz_pole(self):
         pass
+    
+    def info(self):
+        print(f"Figura  oparta na bazowych prametrach: a = {self.a}, b = {self.b}")
